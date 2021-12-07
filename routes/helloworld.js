d8818658f79e419caf25f12d3f7d24ffa26c9f13
고한열 <hanyul97@hotmail.com>
2021-12-07 23:10:15 +0900

    Revert "HelloWorld Test"
    This reverts commit 5bdf0344884a062622a68b31b38c647b3cf56df5.

--- routes/helloworld.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* Hello World Test */
-router.get('/', function(req, res, next) {
-  res.send('hello World!');
-});
-
-module.exports = router;
